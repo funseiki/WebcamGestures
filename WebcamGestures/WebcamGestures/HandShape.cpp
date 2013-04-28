edc1c7c5103383c9d495e16764b62c9eea9c41e7
Vinit Shah <elysiums.shade@gmail.com>
2013-04-28 15:32:42 -0500

    Add a hand shape class

---
+++ WebcamGestures/WebcamGestures/HandShape.cpp
@@ -0,0 +1,14 @@
+#include "HandShape.h"
+
+
+HandShape::HandShape(void)
+{
+	_fingerCount = -1;
+	_angle = -1;
+	_centroid = Point(0,0);
+}
+
+
+HandShape::~HandShape(void)
+{
+}
--- WebcamGestures/WebcamGestures/HandShape.h
+++ WebcamGestures/WebcamGestures/HandShape.h
@@ -5,8 +5,12 @@
 #include<opencv2/opencv.hpp>
 using namespace cv;
 
-struct HandShape
+class HandShape
 {
+public:
+	HandShape(void);
+	~HandShape(void);
+private:
 	int _fingerCount;
 	int _angle;
 	Point _centroid;
