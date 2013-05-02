66c28f55effc8f70d02107b14f090bcd0acde18c
Vinit Shah <elysiums.shade@gmail.com>
2013-05-01 19:38:45 -0500

    Use Ashwin's video

--- WebcamGestures/WebcamGestures/Main.cpp
+++ WebcamGestures/WebcamGestures/Main.cpp
@@ -124,7 +124,7 @@ void CameraLoop()
 
 	// Open the camera
 	//camera.open(0);
-	camera.open("VideoDumpFore.avi");
+	camera.open("VideoDumpForeAsh2.avi");
 	if(!camera.isOpened())
 	{
 		std::cerr << "ERROR: NO CAMERA AVAILABLE!?" << std::endl;
